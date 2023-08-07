4c473e5137c1076f9b10ebed07db7226183dfbf2
Alkida Vaci <alkida.vaci@gmail.com>
2023-08-07 14:17:45 -0400

    Added commponent folder

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="content has-text-centered" style={{}} >
+          <p>
+            © 2023
+          </p>
+      </div>
+    </footer>
+
+  );
+};
+
+export default Footer;
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+
+  return (
+    <div className="hero is-fullheight is-primary">
+      NOT FOUND!!!
+    </div>
+  );
+};
+
+export default NotFound;
