b50ee78602ec8f80770aabc77f123a7481153780
Alkida Vaci <alkida.vaci@gmail.com>
2023-08-07 14:30:55 -0400

    Added pages folder

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const About = () => {
+  return (
+    <div>About Us</div>
+  );
+};
+
+export default About;
+
+
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>home page</div>
+  );
+};
+
+export default Home;
+
+
