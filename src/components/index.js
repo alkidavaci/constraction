4ad21268f9ef6910779b5e2315a6ff5b42d65683
Alkida Vaci <alkida.vaci@gmail.com>
2023-08-07 14:35:59 -0400

    Added project card component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as NotFound } from './NotFound';
-export { default as InventoryList } from './InventoryList';
-export { default as UserCharacter } from './UserCharacter';
+export { default as ProjectCard } from './ProjectCard';
