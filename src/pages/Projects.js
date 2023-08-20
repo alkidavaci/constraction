b2974b2ff99ea3d0fee411d2aeaddea6b6c90123
Alkida Vaci <alkida.vaci@gmail.com>
2023-08-19 22:38:44 -0400

    Initial Project file

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,54 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import ProjectCard from '../components/ProjectCard'; 
+ import './style.css'
 
 const Projects = () => {
+  const projects = [
+    {
+      title: 'Project 1',
+      description: 'Description for Project 1',
+      imageUrl: 'https://static.dezeen.com/uploads/2022/05/mesh-architectures-timber-house-brooklyn_dezeen_1704_hero.jpg',
+    },
+    {
+      title: 'Project 2',
+      description: 'Description for Project 2',
+      imageUrl: 'https://static.dezeen.com/uploads/2022/05/mesh-architectures-timber-house-brooklyn_dezeen_1704_hero.jpg',
+    },
+    {
+      title: 'Project 3',
+      description: 'Description for Project 3',
+      imageUrl: 'https://static.dezeen.com/uploads/2022/05/mesh-architectures-timber-house-brooklyn_dezeen_1704_hero.jpg',
+    },
+    {
+      title: 'Project 1',
+      description: 'Description for Project 1',
+      imageUrl: 'https://static.dezeen.com/uploads/2022/05/mesh-architectures-timber-house-brooklyn_dezeen_1704_hero.jpg',
+    },
+    {
+      title: 'Project 2',
+      description: 'Description for Project 2',
+      imageUrl: 'https://static.dezeen.com/uploads/2022/05/mesh-architectures-timber-house-brooklyn_dezeen_1704_hero.jpg',
+    },
+    {
+      title: 'Project 3',
+      description: 'Description for Project 3',
+      imageUrl: 'https://static.dezeen.com/uploads/2022/05/mesh-architectures-timber-house-brooklyn_dezeen_1704_hero.jpg',
+    },
+    // Add more projects as needed
+  ];
+
   return (
-    <div>Projects</div>
+    <div className="projects-container">
+      {projects.map((project, index) => (
+        <ProjectCard
+          key={index}
+          title={project.title}
+          description={project.description}
+          imageUrl={project.imageUrl}
+        />
+      ))}
+    </div>
   );
 };
 
 export default Projects;
-
-
