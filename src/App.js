8ea25d89f68b45390d97ba2ffb7ed25398ada3a7
Alkida Vaci <alkida.vaci@gmail.com>
2023-08-19 21:43:54 -0400

    Fixed App.js to work with router and route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Note the use of Routes
+
 import { About, Contact, Home, Projects } from './pages';
 import { NotFound, Footer, Header } from './components';
-
 
 function App() {
   return (
     <Router>
-        <Header />
-        
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/about" component={About} />
-          <Route path="*" component={NotFound} />
-        </Switch>
+      <Header />
 
-        <Footer />
+      <Routes> {/* Use the Routes container */}
+        <Route path="/" element={<Home />} /> {/* Note the 'element' prop */}
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+
+const Home = () => {
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="columns is-centered">
+          <div className="column is-6">
+            <div className="box">
+              <h2 className="title has-text-centered">Klodis</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
+
+
